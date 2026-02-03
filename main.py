adab3a799bc4da7180a950834d14c1bbc9881133
Hussain Khan <kamadot373@gmail.com>
2026-02-03 17:27:45 -0500

    Hw bot structure

---
+++ main.py
@@ -0,0 +1,15 @@
+import json
+
+def main():
+    print("The program has started ")
+
+    with open ("config.json", "r") as f:
+        config = json.load(f)
+
+    delay_minutes = config["delay_minutes"]
+    delay_seconds = delay_minutes * 60
+    print("delay_minutes:", delay_minutes)
+    print("delay_seconds:", delay_seconds)
+
+if __name__ == "__main__":
+    main()
