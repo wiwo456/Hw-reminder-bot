3bec4050372b54d483f2d764fd59175477cb7a87
Hussain Khan <kamadot373@gmail.com>
2026-02-08 11:33:30 -0500

    updated detector.py updated main.py updated storage.json

--- detector.py
+++ detector.py
@@ -5,13 +5,36 @@ def detect_hw():
     homework_list.append ({
         "course": "math2100",
         "title":"homework 1",
-        "due":"2026-12-13",
+        "due":"2026-02-13",
+        "time": "11:59 PM",
     })
 
     homework_list.append ({
         "course": "Computer2100",
         "title":"homework 1",
-        "due":"2026-1-13",
+        "due":"2026-2-15",
+        "time": "11:59 PM",
+    })
+
+    homework_list.append({
+        "course": "English1500",
+        "title": "essay",
+        "due": "2026-02-14",
+        "time": "11:59 AM",
+    })
+
+    homework_list.append({
+        "course": "Physics2500",
+        "title": "lab",
+        "due": "2026-02-20",
+        "time": "12:00 PM",
+    })
+    homework_list.append({
+        "course": "Chemistry2500",
+        "title": "lab",
+        "due": "2026-02-20",
+        "time": "12:00 PM",
     })
 
     return homework_list
+
--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import json
 import detector
+from datetime import datetime
+
 
 def main():
     print("The program has started ")
@@ -10,23 +12,37 @@ def main():
     delay_minutes = config["delay_minutes"]
     delay_seconds = delay_minutes * 60
 
+    #this line will read the storage to see if there are any hw and it will send a message if found 
     with open ("storage.json", "r") as f:
         storage = json.load(f)
 
-    homework = detector.detect_hw()
+    reminded_hw = storage["reminded_hw"]
     
+    homework = detector.detect_hw()
+
     if not homework:
         print ("You do not have any assignment due.......")
         return
     else:
         print("\nHomework exits will notify you....\n")
-        print("\nHomework Found:\n")
-
+       
         for hw in homework:
+            hw_id = hw["course"] + " | " + hw["title"] + " | " + hw["due"] + " | " + hw["time"]
+            
+            if hw_id in reminded_hw: #this line does the skip of old hw
+                 continue
+            
+            print(" New homework found ")
             print("Course:", hw['course'])
             print("Title:", hw["title"])
-            print("Due:", hw["due"])
+            print("Due:", hw["due"], hw["time"])
             print("========================")
+
+            reminded_hw.append(hw_id)
+    
+    #this line dumps the already reminded hw to the storage
+    with open ("storage.json", "w") as f:
+        json.dump(storage, f, indent = 4) #indent is to add spacing. 
 
     print("delay_minutes:", delay_minutes)
     print("delay_seconds:", delay_seconds)
